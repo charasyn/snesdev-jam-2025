db035eef1379d189f7358cbab66b4e2d0f1d6b59
Cooper Harasyn <charasyn@outlook.com>
2025-06-15 10:45:40 -0700

    Add test Tiled project

---
+++ experiments/test.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="test" tilewidth="8" tileheight="8" tilecount="512" columns="16">
+ <transformations hflip="1" vflip="1" rotate="0" preferuntransformed="1"/>
+ <image source="four-seasons-tileset.png" width="128" height="256"/>
+ <tile id="0">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
